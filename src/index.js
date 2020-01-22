42d3f5f3711af620b10ed87b9172e81bd864672f
leah Li <leah.li8125@gmail.com>
2020-01-22 13:16:51 +1000

    add nodemon

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Welcome');
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
-app.get('/api/weather/:cc/:city', (req, res) => {
-    // res.send('weather');
-    const { cc, city } = req.params;
-    axios
-        .get(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=64bfcade25a3891088c755f505757b7f`
-        )
-        .then(response => {
-            res.send(response.data);
-        })
-        .catch(err => console.log(err));
-    /*
+app.get("/api/weather/:cc/:city", (req, res) => {
+  // res.send('weather');
+  const { cc, city } = req.params;
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=64bfcade25a3891088c755f505757b7f`
+    )
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => console.log(err));
+  /*
     openweathermap doc: https://openweathermap.org/api 
     -> Current weather data: Api doc 
     -> By city name: API call: api.openweathermap.org/data/2.5/weather?q={city name},{country code}
@@ -27,5 +26,5 @@ app.get('/api/weather/:cc/:city', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('app is listening on port 3000');
-})
+  console.log("app is listening on port 3000");
+});
