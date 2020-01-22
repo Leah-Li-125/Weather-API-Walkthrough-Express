c45a707ebfcdd51a2f5960df7a98e5a33788b2ed
leah Li <leah.li8125@gmail.com>
2020-01-22 12:44:45 +1000

    Step1.3 setup routes (fetch data -openweathermap)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
@@ -8,7 +9,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/weather/:cc/:city', (req, res) => {
-    res.send('weather');
+    // res.send('weather');
+    const { cc, city } = req.params;
+    axios
+        .get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=64bfcade25a3891088c755f505757b7f`
+        )
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(err => console.log(err));
+    /*
+    openweathermap doc: https://openweathermap.org/api 
+    -> Current weather data: Api doc 
+    -> By city name: API call: api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+    */
 });
 
 app.listen(3000, () => {
